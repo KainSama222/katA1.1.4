e99dd6922039c31adec9069ae9d13876381ae76c
MalozenovIgor <kainsamaelis@gmail.com>
2023-05-06 00:50:57 +0200

    1.1.4

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,33 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static Connection conn = null;
+    private static Util instance = null;
+
+    private Util() {
+        try {
+            if (null == conn || conn.isClosed()) {
+                conn = DriverManager
+                        .getConnection("jdbc:mysql://localhost:3306/users", "root", "root");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Util getInstance() {
+        if (null == instance) {
+            instance = new Util();
+        }
+        return instance;
+    }
+
+    public static Connection getConnection() {
+        return conn;
+    }
+
 }
